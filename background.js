425832e1aa91fcf1d6cc17227ee2b7acb95552c1
Robert Reilly <reillyr7@gmail.com>
2019-01-22 20:51:11 -0500

    Basic structure

--- background.js
+++ background.js
@@ -13,14 +13,57 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 });
 
+class HistoryEntry {
+    constructor(url, title, parent, child) {
+        this.url = url;
+        this.title = title;
+        this.parent = parent;
+        this.child = child;
+    }
+}
+
+function addChild(parent, child) {
+    parent.child = child
+    child.parent = parent
+    return child
+}
+
+function showChain(historyEntry) {
+    var chain = "";
+    while (historyEntry) {
+        chain += historyEntry.title + "->";
+        historyEntry = historyEntry.child;
+    }
+    return chain
+}
+
+var previouslyActive = null; // Variable to know of the most recently activated tab
+var head = null; // The origination of a new chain (created when a new tab is created)
+
 chrome.tabs.onCreated.addListener(function (tab) {
-    console.log("Tab:" + tab.url);
+    let tmp = new HistoryEntry(tab.url, tab.title, null, null);
+
+    if (!head) {
+        head = tmp;
+    }
+    previouslyActive = tmp;
+
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    chrome.history.search({ text: '', maxResults: 10 }, function (data) {
-        data.forEach(function (page) {
-            console.log(page.url);
-        });
-    });
+    let tmp = new HistoryEntry(tab.url, tab.title, null, null);
+
+    previouslyActive = addChild(previouslyActive, tmp)
+    console.log(showChain(head));
+
+
+
+
+    // LAST 10 HISTORY ELEMENTS
+    // chrome.history.search({ text: '', maxResults: 10 }, function (data) {
+    //     data.forEach(function (page) {
+    //         console.log(page.url);
+    //     });
+    // });
+
 });
