b6bac785164e980445f44a58b30f5bbd614da0f1
Robert Reilly <reillyr7@gmail.com>
2019-01-23 21:35:44 -0500

    Immediet change

--- popup.js
+++ popup.js
@@ -1,9 +1,13 @@
 chrome.extension.getBackgroundPage().console.log('in popup');
 
 let chain = document.getElementById('chain');
+chain.innerHTML = "New Text"
+
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
+        chrome.extension.getBackgroundPage().console.log('message block');
+
         if (request.subject === "chain") {
             chain.innerHTML = request.content;
         }
